ebcf0f3e71e716b658282431246830aa8c32dd12
Chax <nerfnichtmist@gmail.com>
2022-11-16 18:33:07 +0100

    Hopefully a fix for the fucking firewall.

--- VRPE Installer/VRPE Installer/Buttons.cs
+++ VRPE Installer/VRPE Installer/Buttons.cs
@@ -9,13 +9,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Net.Http;
+using System.Diagnostics;
 
 namespace VRPE_Installer
 {
     public class Buttons
     {
-    public static void FirewallException()
+        public static string ver = string.Empty;
+        public static void FirewallException()
         {
+            HttpClient client = new HttpClient();
+            var fixPath = MainWindow.fixPath;
+            var path = MainWindow.selectedPath;
+            string ver = client.GetStringAsync("https://raw.githubusercontent.com/nerdunit/androidsideloader/master/version").Result;
+            var folderName = $"rookie_{ver}_portable";
+            string finishedPath = $"{path}{fixPath}{folderName}{fixPath}AndroidSideloader_v{ver}.exe";
+            Console.WriteLine($"{path}{fixPath}{folderName}{fixPath}AndroidSideloader_v{ver}.exe");
+            ProcessStartInfo info = new ProcessStartInfo("netsh.exe");
+            info.Arguments = $"advfirewall firewall add allowedprogram {finishedPath}ROOKIE-SIDELOADER ENABLE";
+            System.Diagnostics.Process.Start(info);
         }
     }
 }
